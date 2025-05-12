02089fd38c8dfadd453e87f73bfc60ea37fa72de
Daniela Eloisa <danielaeloisa1@gmail.com>
2025-05-12 09:05:52 -0300

    Criando função para exibir opções do menu.

--- Screen Sound/Program.cs
+++ Screen Sound/Program.cs
@@ -13,4 +13,14 @@ void ExibirMensagemDeboasVindas()
     Console.WriteLine(mensagemDeBoasVinda);
 }
 
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("Digite 1 para registrar uma banda.");
+    Console.WriteLine("Digite 2 para exibir todas as bandas.");
+    Console.WriteLine("Digite 3 para avaliar uma banda.");
+    Console.WriteLine("Digite 4 para exibir media de uma banda.");
+    Console.WriteLine("Digite -1 para sair.");
+}
+
 ExibirMensagemDeboasVindas();
+ExibirOpcoesDoMenu();

6ad9ba49fb37b5e7a09d56cc21598a273aaa8ccc
Daniela Eloisa <danielaeloisa1@gmail.com>
2025-05-12 09:27:18 -0300

    Capturando valores do console. Consvertendo string para inteiro. Tratando valores nulos.

--- Screen Sound/Program.cs
+++ Screen Sound/Program.cs
@@ -20,6 +20,15 @@ void ExibirOpcoesDoMenu()
     Console.WriteLine("Digite 3 para avaliar uma banda.");
     Console.WriteLine("Digite 4 para exibir media de uma banda.");
     Console.WriteLine("Digite -1 para sair.");
+
+    Console.Write("Digite a sua opção: ");
+
+    int opcaoEscolhida = int.Parse(Console.ReadLine()!);
+    if (opcaoEscolhida == 1)
+    {
+        Console.WriteLine("Você digitou a opção 1.");
+
+    }
 }
 
 ExibirMensagemDeboasVindas();

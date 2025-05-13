bc0a370f95b18220b448efea8236ab72f29a1ffe
Daniela Eloisa <danielaeloisa1@gmail.com>
2025-05-13 10:05:48 -0300

    Iniciando curso C#: aplicando a Orientação a Objetos: Criando primeira classe Musica com suas propriedades.

---
+++ Screen Sound/Models/Musica.cs
@@ -0,0 +1,10 @@
+﻿namespace Screen_Sound.Models
+{
+    public class Musica
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string Artista { get; set; } = string.Empty;
+        public int Duracao { get; set; }
+        public bool Disponivel { get; set; }
+    }
+}

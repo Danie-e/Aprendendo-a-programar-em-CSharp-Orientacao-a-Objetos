f31080c8e9bd950645090c3eb1aa979061ba07a9
Danie-e <danielaeloisa1@gmail.com>
2025-08-25 08:58:20 -0300

    Criando rota para listar bandas registradas no banco de dados removendo referencias cíclicas.

--- ScreenSound_Api/Program.cs
+++ ScreenSound_Api/Program.cs
@@ -1,6 +1,15 @@
+using Screen_Sound.Banco;
+using Screen_Sound.Models;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () =>
+{
+    var dal = new DAL<Banda>(new ScreenSoundContext());
+    return dal.Listar();
+});
 
 app.Run();
